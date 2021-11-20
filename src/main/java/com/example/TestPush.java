d533b951c2c214428fda7ec259e0fa41385d654a
dashuaibi <dashuaibi@163.com>
2021-11-20 22:17:45 +0800

    delete file content

--- src/main/java/com/example/TestPush.java
+++ src/main/java/com/example/TestPush.java
@@ -2,13 +2,6 @@ package com.example;
 
 public class TestPush {
   public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        System.out.println("my is update");
-        System.out.println("other is update");
 
-        System.out.println("local commit: my is update2");
-        System.out.println("remote commit: other is update2");
     }
 }
