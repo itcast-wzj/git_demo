24b2fa5466d00f72482271ce4d4f2a300d882932
dashuaibi <dashuaibi@163.com>
2021-11-21 14:57:11 +0800

    local: test reset

--- src/main/java/com/example/TestPush.java
+++ src/main/java/com/example/TestPush.java
@@ -4,5 +4,7 @@ public class TestPush {
   public static void main(String[] args) {
         System.out.println("remote branch: update1");
         System.out.println("remote branch: update2");
+
+        System.out.println("local: test reset");
     }
 }
