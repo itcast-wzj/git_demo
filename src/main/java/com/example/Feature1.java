b0e65a556142ccbb54a17059ac3a3262cf91fe4e
itcast_wzj <46273670+itcast-wzj@users.noreply.github.com>
2022-02-24 22:57:12 +0800

    Update Feature1.java

--- src/main/java/com/example/Feature1.java
+++ src/main/java/com/example/Feature1.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Feature1 {
     public static void main(String[] args) {
-        System.out.println("Feature1");
+        System.out.println("Feature1...");
     }
 }
