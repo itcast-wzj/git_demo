4dcf4c90ed699768bce8d3b87b6ffbfd290456c6
虽然帅，但是菜的cxy <46273670+itcast-wzj@users.noreply.github.com>
2023-11-30 21:39:26 +0800

    Update Feature1.java

--- src/main/java/com/example/Feature1.java
+++ src/main/java/com/example/Feature1.java
@@ -3,5 +3,10 @@ package com.example;
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("Feature1...");
+
+         System.out.println("Feature1...");
+         System.out.println("Feature1...");
+         System.out.println("Feature1...");
+         System.out.println("Feature1...");
     }
 }
